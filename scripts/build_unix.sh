5d9003a0b8f00fcc65002a82510742f91c43f197
conradhuebler <Conrad.Huebler@gmx.net>
2022-02-08 13:44:00 +0100

    update build

--- scripts/build_unix.sh
+++ scripts/build_unix.sh
@@ -8,18 +8,14 @@ git submodule update --recursive
 # check submodules, seems not to work automatically
 
 cd external
-for i in $(ls -d */|grep -v 'xtb'); do cd $i; git checkout master; git submodule init; git submodule update --recursive; git pull; cd ..; done
+for i in $(ls -d */|grep -v 'xtb'); do cd $i; git checkout master || true; git submodule init; git submodule update --recursive; git pull; cd ..; done
 cd xtb
 git checkout main
 git pull
 cd ..
-if [ ! -e "eigen" ]
-	then
-		git clone https://gitlab.com/libeigen/eigen.git/
-	fi
 cd ..
 
 mkdir -p release
 cd release
 cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
-make
+make  -j
--- scripts/build_unix_github.sh
+++ scripts/build_unix_github.sh
@@ -1,36 +1,34 @@
 #!/bin/bash
 set -ex
 
-export CXX="g++-9"
-export CC="gcc-9"
-export FC="gfortran-9"
+#export CXX="g++-9"
+#export CC="gcc-9"
+#export FC="gfortran-9"
 git submodule init
 git submodule update --recursive
 # check submodules, seems not to work automatically
 
 cd external
-for i in $(ls -d */|grep -v 'xtb'); do cd $i; git checkout master; git submodule init; git submodule update --recursive; git pull; cd ..; done
+
+for i in $(ls -d */|grep -v 'xtb'); do cd $i; git checkout master || true ; git submodule init; git submodule update --recursive; git pull; cd ..; done
+
 cd xtb
 git checkout main
 git pull
 cd ..
-if [ ! -e "eigen" ]
-        then
-                git clone https://gitlab.com/libeigen/eigen.git/
-        fi
 cd ..
 
 mkdir -p release_no_xtb
 cd release_no_xtb
 cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo .. -DCMAKE_INSTALL_PREFIX=~/curcuma_no_xtb
-make
+make -j
 make install
 cd ..
 
 mkdir -p release_xtb
 cd release_xtb
 cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo .. -DCOMPILE_XTB=true -DCMAKE_INSTALL_PREFIX=~/curcuma_xtb
-make
+make -j
 make install
 cd ..
 
