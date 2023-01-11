06e124ea1b25455ce776ca334fd1693c0b2adf9c
Conrad Hübler <Conrad.Huebler@gmx.net>
2023-01-11 21:54:01 +0100

    yet another one ...
    Signed-off-by: Conrad Hübler <Conrad.Huebler@gmx.net>

--- scripts/build_unix_github.sh
+++ scripts/build_unix_github.sh
@@ -4,10 +4,7 @@ set -ex
 export CXX="g++-9"
 export CC="gcc-9"
 export FC="gfortran-9"
-
-#apt-get install libblas-dev checkinstall
-sudo apt-get install liblapacke-dev checkinstall
-
+export CMAKE_PREFIX_PATH=/usr/:/usr/lib/:/usr/include
 git submodule init
 git submodule update --recursive
 # check submodules, seems not to work automatically
@@ -41,7 +38,7 @@ cd ..
 
 mkdir -p release_xtb
 cd release_xtb
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo .. -DUSE_XTB=true -DUSE_TBLITE=true -DUSE_D3=true -DUSE_D4=true -DCMAKE_INSTALL_PREFIX=~/curcuma_xtb
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo .. -DUSE_XTB=true -DUSE_TBLITE=true -DUSE_D3=true -DUSE_D4=true -DCMAKE_INSTALL_PREFIX=~/curcuma_xtb -DCMAKE_PREFIX_PATH=/usr/:/usr/lib/:/usr/include
 make 
 make install
 cd ..
